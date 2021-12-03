1486a2cc447d37ea1d0faf27334311858bc13b92
Patrick Rogers <patrick@donotforget.xyz>
2021-12-02 22:24:32 -0800

    ugly code

---
+++ day03/main.ml
@@ -0,0 +1,66 @@
+open Core
+
+let binary_char_to_int c = if Char.equal c '0' then 0 else 1
+
+let input =
+  In_channel.read_lines "./input/day03.txt"
+  |> List.map ~f:(fun s ->
+         s |> String.to_list |> List.map ~f:binary_char_to_int )
+
+
+let binary_to_int =
+  List.fold ~init:0 ~f:(fun acc a -> Int.bit_or (Int.shift_left acc 1) a)
+
+
+let part1 =
+  let counts =
+    input
+    |> List.transpose_exn
+    |> List.map ~f:(fun l ->
+           List.fold
+             ~init:(0, 0)
+             ~f:(fun (c0, c1) c -> if c = 0 then (c0 + 1, c1) else (c0, c1 + 1))
+             l )
+  in
+  let gamma =
+    counts
+    |> List.map ~f:(fun (x, y) -> if x > y then 0 else 1)
+    |> binary_to_int
+  in
+  let epsilon =
+    counts
+    |> List.map ~f:(fun (x, y) -> if x < y then 0 else 1)
+    |> binary_to_int
+  in
+  gamma * epsilon
+
+
+let part2 =
+  let filter ls n chooser =
+    let len = List.length ls in
+    let cs = List.map ~f:(fun l -> List.nth_exn l n) ls in
+    let c0 = List.count ~f:(Int.equal 0) cs in
+    let choice = chooser c0 len in
+    List.filter ~f:(fun l -> Int.equal choice (List.nth_exn l n)) ls
+  in
+  let rec helper l n chooser =
+    match l with
+    | [] ->
+        raise (Failure "?")
+    | [ ans ] ->
+        ans
+    | _ ->
+        helper (filter l n chooser) (n + 1) chooser
+  in
+  let o2 =
+    helper input 0 (fun c0 len -> if c0 > len / 2 then 0 else 1)
+    |> binary_to_int
+  in
+  let co2 =
+    helper input 0 (fun c0 len -> if c0 <= len / 2 then 0 else 1)
+    |> binary_to_int
+  in
+  o2 * co2
+
+
+let () = Printf.printf "part1=%d; part2=%d" part1 part2
