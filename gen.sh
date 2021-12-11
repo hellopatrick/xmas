4445b7a346e4b0127771c01801b4300e1e22e2bb
Patrick Rogers <hellopatrick@users.noreply.github.com>
2021-12-11 13:13:29 -0800

    add gen

---
+++ gen.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+num=$1
+day="day$num"
+
+# solution
+
+mkdir -p "$day"
+
+cat > "$day"/dune <<EOF
+(executable
+ (public_name $day)
+ (name main)
+ (libraries xmas core))
+EOF
+
+cat > "$day"/main.ml <<EOF
+open Core
+
+let _ = Printf.printf "part1=;part2="
+EOF
+
+# test files
+
+mkdir -p test/"$day".t
+
+touch test/"$day".t/test
+touch test/"$day".t/input
+
+cat > test/"$day".t/run.t <<EOF
+https://adventofcode.com/2021/day/$num
+
+  $ $day < test
+  part1=;part2=
+
+  $ $day < input
+  part1=;part2=
+EOF
+
+cat >> test/dune <<EOF
+
+(cram
+ (applies_to $day)
+ (deps %{bin:$day}))
+EOF
+
+dune build
