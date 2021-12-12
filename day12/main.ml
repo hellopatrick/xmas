0b3278665476320d104b58cdc9cfc5b78f889e24
Patrick Rogers <hellopatrick@users.noreply.github.com>
2021-12-12 10:25:03 -0800

    part1 on a plane

--- day12/main.ml
+++ day12/main.ml
@@ -1,3 +1,76 @@
 open Core
 
-let _ = Printf.printf "part1=;part2="
+let is_uppercase str = String.equal str (String.uppercase str)
+
+module Cave = struct
+  type t = Large of string | Small of string [@@deriving show, eq, ord]
+  type link = { start : t; finish : t }
+
+  (* now stringable *)
+  let of_string = function
+    | str when is_uppercase str -> Large str
+    | str -> Small str
+
+  let to_string = show
+
+  (* how to do this via Sexapble.of_stringable? *)
+  let t_of_sexp sexp =
+    match sexp with
+    | Sexp.Atom s -> ( try of_string s with exn -> of_sexp_error_exn exn sexp)
+    | Sexp.List _ ->
+        of_sexp_error
+          "Sexpable.Of_stringable.t_of_sexp expected an atom, but got a list"
+          sexp
+
+  let sexp_of_t t = Sexp.Atom (to_string t)
+
+  let parse l =
+    match String.split ~on:'-' l with
+    | s :: e :: _ -> { start = of_string s; finish = of_string e }
+    | _ -> failwith "unreachable"
+
+  let start = Small "start"
+  let finish = Small "end"
+  let is_end = equal finish
+end
+
+module NodeMap = Map.Make (Cave)
+module NodeSet = Set.Make (Cave)
+
+let create_adjacency_list links =
+  List.fold links ~init:NodeMap.empty ~f:(fun m Cave.{ start; finish } ->
+      let m =
+        NodeMap.update m start ~f:(function
+          | Some nodes -> NodeSet.add nodes finish
+          | None -> NodeSet.of_list [ finish ])
+      in
+      NodeMap.update m finish ~f:(function
+        | Some nodes -> NodeSet.add nodes start
+        | None -> NodeSet.of_list [ start ]))
+
+let input = In_channel.input_lines In_channel.stdin |> List.map ~f:Cave.parse
+let adj_list = create_adjacency_list input
+
+let dfs m =
+  let find = NodeMap.find_exn m in
+  let rec aux node visited path complete =
+    if Cave.is_end node then List.rev (node :: path) :: complete
+    else
+      let adjacent = find node in
+      let next = NodeSet.diff adjacent visited in
+      if NodeSet.is_empty next then complete
+      else
+        let visited =
+          match node with
+          | Cave.Large _ -> visited
+          | _ -> NodeSet.add visited node
+        in
+        let path = node :: path in
+        NodeSet.fold next ~init:complete ~f:(fun complete node ->
+            aux node visited path complete)
+  in
+  aux Cave.start NodeSet.empty [] []
+
+let paths = dfs adj_list
+let part1 = List.length paths
+let _ = Printf.printf "part1=%d;part2=" part1
--- test/day12.t/run.t
+++ test/day12.t/run.t
@@ -1,7 +1,13 @@
 https://adventofcode.com/2021/day/12
 
-  $ day12 < test
-  part1=;part2=
+  $ day12 < test-1
+  part1=10;part2=
+
+  $ day12 < test-2
+  part1=19;part2=
+
+  $ day12 < test-3
+  part1=226;part2=
 
   $ day12 < input
-  part1=;part2=
+  part1=4707;part2=
