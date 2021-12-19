c81dc0943ea10eadfdd9553101d79f8a16d943ba
Patrick Rogers <hellopatrick@users.noreply.github.com>
2021-12-19 13:14:46 -0500

    day19

---
+++ day19/main.ml
@@ -0,0 +1,120 @@
+open Core
+
+module T3 = struct
+  type t = int * int * int [@@deriving show, eq, ord, sexp]
+
+  let diff (x0, y0, z0) (x1, y1, z1) = (x0 - x1, y0 - y1, z0 - z1)
+
+  let add (x0, y0, z0) (x1, y1, z1) = (x0 + x1, y0 + y1, z0 + z1)
+
+  let manhattan_dist a b =
+    let dx, dy, dz = diff a b in
+    Int.abs dx + Int.abs dy + Int.abs dz
+end
+
+module CS = Set.Make (T3)
+
+let show_cs t = CS.sexp_of_t t |> Sexp.to_string_hum
+
+let parse lines =
+  List.group lines ~break:(fun a b -> String.is_empty a || String.is_empty b)
+  |> List.filter_map ~f:(function
+       | [] | [""] ->
+           None
+       | header :: probes when String.is_prefix header ~prefix:"---" ->
+           Some probes
+       | _ ->
+           failwith "unknown group" )
+  |> List.map
+       ~f:
+         (List.map ~f:(fun line ->
+              Scanf.sscanf line "%d,%d,%d" (fun x y z -> (x, y, z)) ) )
+  |> List.map ~f:CS.of_list
+
+let rotations =
+  [ (fun (x, y, z) -> (x, y, z))
+  ; (fun (x, y, z) -> (x, z, -y))
+  ; (fun (x, y, z) -> (x, -y, -z))
+  ; (fun (x, y, z) -> (x, -z, y))
+  ; (fun (x, y, z) -> (-x, y, -z))
+  ; (fun (x, y, z) -> (-x, z, y))
+  ; (fun (x, y, z) -> (-x, -y, z))
+  ; (fun (x, y, z) -> (-x, -z, -y))
+  ; (fun (x, y, z) -> (-y, x, z))
+  ; (fun (x, y, z) -> (-z, x, -y))
+  ; (fun (x, y, z) -> (y, x, -z))
+  ; (fun (x, y, z) -> (z, x, y))
+  ; (fun (x, y, z) -> (y, -x, z))
+  ; (fun (x, y, z) -> (z, -x, -y))
+  ; (fun (x, y, z) -> (-y, -x, -z))
+  ; (fun (x, y, z) -> (-z, -x, y))
+  ; (fun (x, y, z) -> (-z, y, x))
+  ; (fun (x, y, z) -> (y, z, x))
+  ; (fun (x, y, z) -> (z, -y, x))
+  ; (fun (x, y, z) -> (-y, -z, x))
+  ; (fun (x, y, z) -> (z, y, -x))
+  ; (fun (x, y, z) -> (-y, z, -x))
+  ; (fun (x, y, z) -> (-z, -y, -x))
+  ; (fun (x, y, z) -> (y, -z, -x)) ]
+
+module IntSet = Set.Make (Int)
+
+let find_offset known unknown =
+  CS.find_map unknown ~f:(fun pt ->
+      let possible_offsets = CS.map known ~f:(fun a -> T3.diff a pt) in
+      CS.find_map possible_offsets ~f:(fun offset ->
+          let transformed_unknown =
+            CS.map unknown ~f:(fun pt -> T3.add pt offset)
+          in
+          let common = CS.inter transformed_unknown known in
+          let len = CS.length common in
+          if len >= 12 then Some (offset, transformed_unknown) else None ) )
+
+let fit known_beacons unknown_scan =
+  List.find_map rotations ~f:(fun rot ->
+      let transformed = CS.map unknown_scan ~f:rot in
+      find_offset known_beacons transformed )
+
+let find_next_alignment known_beacons unknown_scans =
+  let rec aux unknown_scans still_unknown_scans =
+    match unknown_scans with
+    | [] ->
+        failwith "unknown_scans = empty_list"
+    | attempt :: tl -> (
+      match fit known_beacons attempt with
+      | Some (offset, adjusted) ->
+          (offset, adjusted, tl @ still_unknown_scans)
+      | None ->
+          aux tl (attempt :: still_unknown_scans) )
+  in
+  aux unknown_scans []
+
+let rec solve offsets known_beacons unknown_scans =
+  match unknown_scans with
+  | [] ->
+      (offsets, known_beacons)
+  | _ ->
+      let offset, found_beacons, leftovers =
+        find_next_alignment known_beacons unknown_scans
+      in
+      solve (offset :: offsets) (CS.union known_beacons found_beacons) leftovers
+
+let process = function
+  | [] ->
+      failwith "must have at least one reading"
+  | hd :: tl ->
+      solve [(0, 0, 0)] hd tl
+
+let input = Stdio.(In_channel.input_lines stdin) |> parse
+
+let offsets, known_beacons = process input
+
+let part1 = CS.length known_beacons
+
+let part2 =
+  List.cartesian_product offsets offsets
+  |> List.map ~f:(fun (a, b) -> T3.manhattan_dist a b)
+  |> List.max_elt ~compare:Int.compare
+  |> Option.value_exn
+
+let _ = Printf.printf "part1=%d;part2=%d" part1 part2
---
+++ test/day19.t/run.t
@@ -0,0 +1,7 @@
+https://adventofcode.com/2021/day/19
+
+  $ day19 < test
+  part1=79;part2=3621
+
+  $ day19 < input
+  part1=390;part2=13327
