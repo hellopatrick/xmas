b9952fec6b78eedcac18988d5c619481c7774bd9
Patrick Rogers <hellopatrick@users.noreply.github.com>
2021-12-22 02:28:30 -0500

    clean

--- day22/main.ml
+++ day22/main.ml
@@ -1,12 +1,7 @@
 open Core
 
-module M = struct
-  type t = int * int * int [@@deriving show, ord, eq, sexp, hash]
-end
-
 module Cuboid = struct
   type t = {xmin: int; xmax: int; ymin: int; ymax: int; zmin: int; zmax: int}
-  [@@deriving show, ord, eq, sexp, hash]
 
   let volume t =
     (t.xmax - t.xmin + 1) * (t.ymax - t.ymin + 1) * (t.zmax - t.zmin + 1)
@@ -30,12 +25,9 @@ end
 
 module Instruction = struct
   type t = On of Cuboid.t | Off of Cuboid.t
-  [@@deriving show, ord, eq, sexp, hash]
 
   let cuboid t = match t with On c -> c | Off c -> c
 end
-
-module CoordTable = Hashtbl.Make (M)
 
 let parse line =
   if String.is_prefix line ~prefix:"on" then
@@ -65,33 +57,7 @@ let rec count_untouched cube rest =
   let touched = aux rest 0 in
   volume - touched
 
-let part1 =
-  let start = `inclusive and stop = `inclusive in
-  let m = CoordTable.create () in
-  let () =
-    List.iter input ~f:(fun i ->
-        let on, Cuboid.{xmin; xmax; ymin; ymax; zmin; zmax} =
-          match i with
-          | On {xmin; xmax; ymin; ymax; zmin; zmax} ->
-              (true, {xmin; xmax; ymin; ymax; zmin; zmax})
-          | Off {xmin; xmax; ymin; ymax; zmin; zmax} ->
-              (false, {xmin; xmax; ymin; ymax; zmin; zmax})
-        in
-        let xs =
-          Sequence.range ~start ~stop (Int.max (-50) xmin) (Int.min 50 xmax)
-        and ys =
-          Sequence.range ~start ~stop (Int.max (-50) ymin) (Int.min 50 ymax)
-        and zs =
-          Sequence.range ~start ~stop (Int.max (-50) zmin) (Int.min 50 zmax)
-        in
-        Sequence.iter xs ~f:(fun x ->
-            Sequence.iter ys ~f:(fun y ->
-                Sequence.iter zs ~f:(fun z ->
-                    CoordTable.update m (x, y, z) ~f:(fun _ -> on) ) ) ) )
-  in
-  CoordTable.count m ~f:ident
-
-let part2 =
+let solve cubes =
   let rec aux cubes cnt =
     match cubes with
     | [] ->
@@ -101,6 +67,25 @@ let part2 =
     | _ :: tl ->
         aux tl cnt
   in
-  aux input 0
+  aux cubes 0
+
+let part1 =
+  let bounds =
+    Cuboid.{xmin= -50; xmax= 50; ymin= -50; ymax= 50; zmin= -50; zmax= 50}
+  in
+  let constrained =
+    input
+    (* so ugly... *)
+    |> List.filter_map ~f:(function
+         | Instruction.On c ->
+             Cuboid.intersection c bounds
+             |> Option.map ~f:(fun i -> Instruction.On i)
+         | Instruction.Off c ->
+             Cuboid.intersection c bounds
+             |> Option.map ~f:(fun i -> Instruction.Off i) )
+  in
+  solve constrained
+
+let part2 = solve input
 
 let _ = Printf.printf "part1=%d;part2=%d" part1 part2
