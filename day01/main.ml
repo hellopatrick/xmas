bd855e8d5c5e22cc61223ad06cb6da042b29df9d
Patrick Rogers <hellopatrick@users.noreply.github.com>
2022-12-01 06:33:47 -0800

    refactor

--- day01/main.ml
+++ day01/main.ml
@@ -3,21 +3,17 @@ open Xmas.Option
 
 let input = In_channel.(input_lines stdin)
 
-let parse_elves l =
+let sorted_elves_capacity l =
   List.map l ~f:(fun l -> int_of_string_opt l |? lazy 0)
   |> List.group ~break:(fun a _ -> a = 0)
+  |> List.map ~f:Xmas.Enum.sum
+  |> List.sort ~compare:Int.compare
+  |> List.rev
 
-let part1 l =
-  parse_elves l |> List.map ~f:Xmas.Enum.sum
-  |> List.max_elt ~compare:Int.compare
-  |? lazy 0
+let elves = sorted_elves_capacity input
 
-let part2 l =
-  let elves =
-    parse_elves l |> List.map ~f:Xmas.Enum.sum
-    |> List.sort ~compare:Int.compare
-    |> List.rev
-  in
-  List.take elves 3 |> Xmas.Enum.sum
+let part1 = List.hd_exn elves
 
-let _ = Printf.printf "part1=%d;part2=%d" (part1 input) (part2 input)
+let part2 = List.take elves 3 |> Xmas.Enum.sum
+
+let _ = Printf.printf "part1=%d;part2=%d" part1 part2
