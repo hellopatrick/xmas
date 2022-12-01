f9de7d424182b2c4f55424485662af48056cfb1c
Patrick Rogers <hellopatrick@users.noreply.github.com>
2022-11-30 21:18:20 -0800

    feat: day01

--- day01/main.ml
+++ day01/main.ml
@@ -2,4 +2,26 @@ open Core
 
 let input = In_channel.(input_lines stdin)
 
-let _ = Printf.printf "part1=;part2=3"
+let part1 lines =
+  let lines =
+    List.map lines ~f:(fun l ->
+        if String.is_empty l then 0 else Int.of_string l )
+  in
+  let elves = List.group lines ~break:(fun _ b -> b = 0) in
+  let elves = List.filter elves ~f:(fun l -> not (List.is_empty l)) in
+  let sums = List.map elves ~f:(fun l -> List.fold l ~init:0 ~f:Int.( + )) in
+  List.max_elt sums ~compare:Int.compare |> Option.value ~default:0
+
+let part2 lines =
+  let lines =
+    List.map lines ~f:(fun l ->
+        if String.is_empty l then 0 else Int.of_string l )
+  in
+  let elves = List.group lines ~break:(fun _ b -> b = 0) in
+  let elves = List.filter elves ~f:(fun l -> not (List.is_empty l)) in
+  let sums = List.map elves ~f:(fun l -> List.fold l ~init:0 ~f:Int.( + )) in
+  let sums = List.sort sums ~compare:Int.compare in
+  let top3 = List.drop sums (List.length sums - 3) in
+  List.fold top3 ~init:0 ~f:Int.( + )
+
+let _ = Printf.printf "part1=%d;part2=%d" (part1 input) (part2 input)
--- day01/test.t/run.t
+++ day01/test.t/run.t
@@ -1,7 +1,7 @@
 https://adventofcode.com/2022/day/01
 
   $ day01 < test
-  part1=;part2=3
+  part1=24000;part2=45000
 
   $ day01 < input
-  part1=;part2=3
+  part1=72511;part2=212117
