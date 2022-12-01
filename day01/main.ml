e208ddf5d9eb04d93e41abc7c105f743b848a3ca
Patrick Rogers <hellopatrick@users.noreply.github.com>
2022-11-30 21:26:45 -0800

    refactor

--- day01/main.ml
+++ day01/main.ml
@@ -2,26 +2,24 @@ open Core
 
 let input = In_channel.(input_lines stdin)
 
-let part1 lines =
+let parse_elves l =
   let lines =
-    List.map lines ~f:(fun l ->
-        if String.is_empty l then 0 else Int.of_string l )
+    List.map l ~f:(fun l -> int_of_string_opt l |> Option.value ~default:0)
   in
-  let elves = List.group lines ~break:(fun _ b -> b = 0) in
-  let elves = List.filter elves ~f:(fun l -> not (List.is_empty l)) in
-  let sums = List.map elves ~f:(fun l -> List.fold l ~init:0 ~f:Int.( + )) in
-  List.max_elt sums ~compare:Int.compare |> Option.value ~default:0
+  List.group lines ~break:(fun _ b -> b = 0)
 
-let part2 lines =
-  let lines =
-    List.map lines ~f:(fun l ->
-        if String.is_empty l then 0 else Int.of_string l )
+let part1 l =
+  let elves = parse_elves l in
+  let carrying = List.map elves ~f:Xmas.Enum.sum in
+  List.max_elt carrying ~compare:Int.compare |> Option.value ~default:0
+
+let part2 l =
+  let elves = parse_elves l in
+  let carrying = List.map elves ~f:Xmas.Enum.sum in
+  let carrying_sorted =
+    List.sort carrying ~compare:(fun a b -> Int.neg (Int.compare a b))
   in
-  let elves = List.group lines ~break:(fun _ b -> b = 0) in
-  let elves = List.filter elves ~f:(fun l -> not (List.is_empty l)) in
-  let sums = List.map elves ~f:(fun l -> List.fold l ~init:0 ~f:Int.( + )) in
-  let sums = List.sort sums ~compare:Int.compare in
-  let top3 = List.drop sums (List.length sums - 3) in
-  List.fold top3 ~init:0 ~f:Int.( + )
+  let top_three = List.take carrying_sorted 3 in
+  Xmas.Enum.sum top_three
 
 let _ = Printf.printf "part1=%d;part2=%d" (part1 input) (part2 input)
---
+++ lib/enum.ml
@@ -0,0 +1,3 @@
+open Base
+
+let sum = List.fold ~init:0 ~f:Int.( + )
