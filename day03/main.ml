cc4d9aa810c8a9f68779099bd32d492d59b8f922
Patrick Rogers <hellopatrick@users.noreply.github.com>
2022-12-02 22:37:56 -0800

    fix

--- day03/main.ml
+++ day03/main.ml
@@ -9,31 +9,35 @@ let priority c =
   | 'A' .. 'Z' ->
       -38 + Char.to_int c
   | _ ->
-      0
+      assert false
+
+let ( % ) = Fn.compose
+
+let char_set_of_string = Char.Set.of_list % String.to_list_rev
 
 let part1 input =
   List.map
     ~f:(fun str ->
-      let len = String.length str in
-      let half = len / 2 in
-      (String.sub str ~pos:0 ~len:half, String.sub str ~pos:half ~len:half) )
+      let full_len = String.length str in
+      let len = full_len / 2 in
+      (String.sub str ~pos:0 ~len, String.sub str ~pos:len ~len) )
     input
-  |> List.map ~f:(fun tuple -> Tuple2.map tuple ~f:String.to_list_rev)
-  |> List.map ~f:(fun tuple -> Tuple2.map tuple ~f:Char.Set.of_list)
+  |> List.map ~f:(fun tuple -> Tuple2.map tuple ~f:char_set_of_string)
   |> List.map ~f:(fun (first, second) -> Char.Set.inter first second)
-  |> List.map ~f:Char.Set.choose_exn
-  |> List.map ~f:priority |> Xmas.Enum.sum
+  |> List.fold ~init:0 ~f:(fun acc c -> acc + priority (Char.Set.choose_exn c))
 
 let part2 input =
   let rec aux rucksacks common =
     match rucksacks with
     | a :: b :: c :: tl ->
-        let c = Char.Set.inter (Char.Set.inter a b) c |> Char.Set.choose_exn in
-        aux tl (c :: common)
+        let cc =
+          Char.Set.inter a b |> Char.Set.inter c |> Char.Set.choose_exn
+        in
+        aux tl (cc :: common)
     | _ ->
         common
   in
-  let i = List.map ~f:(fun l -> Char.Set.of_list (String.to_list l)) input in
-  aux i [] |> List.map ~f:priority |> Xmas.Enum.sum
+  let i = List.map ~f:char_set_of_string input in
+  aux i [] |> List.fold ~init:0 ~f:(fun acc c -> acc + priority c)
 
 let _ = Printf.printf "part1=%d;part2=%d" (part1 input) (part2 input)
