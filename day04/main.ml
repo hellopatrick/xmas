350f1fe1db5a00188e0b161cae202453fda5ac29
Patrick Rogers <hellopatrick@users.noreply.github.com>
2022-12-04 10:02:29 -0800

    sequence

--- day04/main.ml
+++ day04/main.ml
@@ -1,11 +1,11 @@
 open Core
 
-let input = In_channel.(input_lines stdin)
+let input = In_channel.(input_lines stdin) |> Sequence.of_list
 
 module Range = struct
   type t = {first: int; last: int}
 
-  let parse b = Scanf.bscanf b "%d-%d" (fun first last -> {first; last})
+  let parse b = Scanf.bscanf b "%i-%i" (fun first last -> {first; last})
 
   let includes t s = t.first <= s.first && t.last >= s.last
 
@@ -16,13 +16,13 @@ let parse line =
   Scanf.sscanf line "%r,%r" Range.parse Range.parse (fun r1 r2 -> (r1, r2))
 
 let part1 input =
-  input |> List.map ~f:parse
-  |> List.fold ~init:0 ~f:(fun acc (a, b) ->
+  input |> Sequence.map ~f:parse
+  |> Sequence.fold ~init:0 ~f:(fun acc (a, b) ->
          acc + Bool.to_int (Range.includes a b || Range.includes b a) )
 
 let part2 input =
-  input |> List.map ~f:parse
-  |> List.fold ~init:0 ~f:(fun acc (a, b) ->
+  input |> Sequence.map ~f:parse
+  |> Sequence.fold ~init:0 ~f:(fun acc (a, b) ->
          acc + Bool.to_int (Range.overlap a b) )
 
 let _ = Printf.printf "part1=%d;part2=%d" (part1 input) (part2 input)
