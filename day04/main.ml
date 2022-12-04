42589ff42343adb241af85d1165924a6b962de10
Patrick Rogers <hellopatrick@users.noreply.github.com>
2022-12-04 10:13:04 -0800

    keep list

--- day04/main.ml
+++ day04/main.ml
@@ -1,6 +1,6 @@
 open Core
 
-let input = In_channel.(input_lines stdin) |> Sequence.of_list
+let input = In_channel.(input_lines stdin)
 
 module Range = struct
   type t = {first: int; last: int}
@@ -16,13 +16,13 @@ let parse line =
   Scanf.sscanf line "%r,%r" Range.parse Range.parse (fun r1 r2 -> (r1, r2))
 
 let part1 input =
-  input |> Sequence.map ~f:parse
-  |> Sequence.fold ~init:0 ~f:(fun acc (a, b) ->
+  input |> List.map ~f:parse
+  |> List.fold ~init:0 ~f:(fun acc (a, b) ->
          acc + Bool.to_int (Range.includes a b || Range.includes b a) )
 
 let part2 input =
-  input |> Sequence.map ~f:parse
-  |> Sequence.fold ~init:0 ~f:(fun acc (a, b) ->
+  input |> List.map ~f:parse
+  |> List.fold ~init:0 ~f:(fun acc (a, b) ->
          acc + Bool.to_int (Range.overlap a b) )
 
 let _ = Printf.printf "part1=%d;part2=%d" (part1 input) (part2 input)
