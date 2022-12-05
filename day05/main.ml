8b8b01b5c64634ae971184650ca12f47d8385d01
Patrick Rogers <hellopatrick@users.noreply.github.com>
2022-12-04 21:52:26 -0800

    done

---
+++ day05/main.ml
@@ -0,0 +1,47 @@
+open Core
+
+let input = In_channel.(input_lines stdin)
+
+module Move = struct
+  type t = {src: int; dst: int; amt: int}
+
+  let parse s =
+    Scanf.sscanf s "move %d from %d to %d" (fun amt src dst ->
+        {src= src - 1; dst= dst - 1; amt} )
+end
+
+let parse lines =
+  let stacks, moves =
+    List.split_while lines ~f:(fun s -> not (String.equal s "--"))
+  in
+  let stacks = List.map stacks ~f:String.to_list in
+  let moves = List.tl_exn moves in
+  let moves = List.map moves ~f:Move.parse in
+  (Array.of_list stacks, moves)
+
+let solve f stack moves =
+  let rec aux moves =
+    match moves with
+    | [] ->
+        ()
+    | mv :: tl ->
+        let Move.{src; dst; amt} = mv in
+        let s = stack.(src) in
+        let d = stack.(dst) in
+        let s' = List.take s amt in
+        Array.set stack src (List.drop s amt) ;
+        Array.set stack dst (List.concat [f s'; d]) ;
+        aux tl
+  in
+  let _ = aux moves in
+  Array.map stack ~f:List.hd_exn |> Array.to_list |> String.of_char_list
+
+let part1 input =
+  let stack, moves = parse input in
+  solve List.rev stack moves
+
+let part2 input =
+  let stack, moves = parse input in
+  solve Fn.id stack moves
+
+let _ = Printf.printf "part1=%s;part2=%s" (part1 input) (part2 input)
