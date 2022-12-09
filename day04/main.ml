ec1ee0f047602ae07cd606df009c06f8e766b1ac
Patrick Rogers <hellopatrick@users.noreply.github.com>
2022-12-09 07:39:14 -0800

    day 04 without core

--- day04/main.ml
+++ day04/main.ml
@@ -1,6 +1,6 @@
-open Core
+open Containers
 
-let input = In_channel.(input_lines stdin)
+let input = IO.(read_lines_l stdin)
 
 module Range = struct
   type t = {first: int; last: int}
@@ -16,13 +16,14 @@ let parse line =
   Scanf.sscanf line "%r,%r" Range.parse Range.parse (fun r1 r2 -> (r1, r2))
 
 let part1 input =
-  input |> List.map ~f:parse
-  |> List.fold ~init:0 ~f:(fun acc (a, b) ->
+  input |> List.map parse
+  |> List.fold_left
+       (fun acc (a, b) ->
          acc + Bool.to_int (Range.includes a b || Range.includes b a) )
+       0
 
 let part2 input =
-  input |> List.map ~f:parse
-  |> List.fold ~init:0 ~f:(fun acc (a, b) ->
-         acc + Bool.to_int (Range.overlap a b) )
+  input |> List.map parse
+  |> List.fold_left (fun acc (a, b) -> acc + Bool.to_int (Range.overlap a b)) 0
 
 let _ = Printf.printf "part1=%d;part2=%d" (part1 input) (part2 input)
