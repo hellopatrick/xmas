f995d6be82506db883868c63ff34620f24e3ff99
Patrick Rogers <hellopatrick@users.noreply.github.com>
2022-12-09 07:23:54 -0800

    feat: create common coordinate module

--- day08/main.ml
+++ day08/main.ml
@@ -2,16 +2,7 @@ open Containers
 
 let input = IO.read_lines_l stdin
 
-module C = struct
-  type t = int * int
-
-  let compare (t0, t1) (s0, s1) =
-    match Int.compare t0 s0 with 0 -> Int.compare t1 s1 | v -> v
-
-  let add (t0, t1) (s0, s1) = (t0 + s0, t1 + s1)
-
-  let to_string (x, y) = Printf.sprintf "(%d, %d)" x y
-end
+module C = Xmas.Coordinate
 
 module Map = struct
   include Map.Make (C)
--- day09/main.ml
+++ day09/main.ml
@@ -21,16 +21,7 @@ let parse input =
   |> List.flatten
 
 module C = struct
-  type t = int * int
-
-  let zero _ = (0, 0)
-
-  let compare (t0, t1) (s0, s1) =
-    match Int.compare t0 s0 with 0 -> Int.compare t1 s1 | v -> v
-
-  let add (t0, t1) (s0, s1) = (t0 + s0, t1 + s1)
-
-  let to_string (x, y) = Printf.sprintf "(%d, %d)" x y
+  include Xmas.Coordinate
 
   let touching (hx, hy) (tx, ty) =
     Int.abs (hx - tx) <= 1 && Int.abs (hy - ty) <= 1
