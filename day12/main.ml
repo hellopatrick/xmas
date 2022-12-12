0e0d8baf3032df273527798b18b2455f13e0e38a
Patrick Rogers <hellopatrick@users.noreply.github.com>
2022-12-12 05:50:29 -0800

    simplify

--- day12/main.ml
+++ day12/main.ml
@@ -2,18 +2,17 @@ open Containers
 
 let input = IO.read_lines_l stdin
 
-module M = Map.Make (Xmas.Coordinate)
-module S = Set.Make (Xmas.Coordinate)
+module C = Xmas.Coordinate
+module M = Map.Make (C)
+module S = Set.Make (C)
 
 let parse i =
   List.foldi
-    (fun acc y line ->
-      line |> String.to_list
-      |> List.foldi (fun acc x c -> M.add (x, y) c acc) acc )
+    (fun acc y line -> String.foldi (fun acc x c -> M.add (x, y) c acc) acc line)
     M.empty i
 
-let starts m c =
-  M.filter (fun _ h -> Char.equal h c) m |> M.keys |> List.of_iter
+let starts c m =
+  m |> M.filter (fun _ h -> Char.equal h c) |> M.keys |> List.of_iter
 
 let goal m = m |> M.filter (fun _ h -> Char.equal h 'E') |> M.choose |> fst
 
@@ -27,20 +26,22 @@ let height = function
   | c ->
       Char.code c
 
+let get a = Option.get_exn_or "not found" a
+
 let solve m s e =
   let q = Queue.create () in
-  let rec aux d =
+  let rec aux ds =
     if Queue.is_empty q then Int.max_int
     else
       let loc = Queue.pop q in
-      let h = M.get loc m |> Option.get_exn_or "unreach." in
-      let dist = M.get loc d |> Option.get_exn_or "unreach." in
-      if Xmas.Coordinate.equal loc e then dist
+      let h = M.get loc m |> get in
+      let dist = M.get loc ds |> get in
+      if C.equal loc e then dist
       else
         let n =
           List.filter
             (fun l ->
-              match (M.get l m, M.get l d) with
+              match (M.get l m, M.get l ds) with
               | None, _ | _, Some _ ->
                   false
               | Some h', _ ->
@@ -49,23 +50,23 @@ let solve m s e =
             (neighbors loc)
         in
         Queue.add_seq q (List.to_seq n) ;
-        let d =
+        let ds' =
           List.fold_left
             (fun acc c -> M.update c (Fun.const (Some (dist + 1))) acc)
-            d n
+            ds n
         in
-        aux d
+        aux ds'
   in
   let _ = Queue.add s q in
   aux (M.of_list [(s, 0)])
 
 let part1 m =
-  let start = starts m 'S' |> List.hd in
+  let start = starts 'S' m |> List.hd in
   let goal = goal m in
   solve m start goal
 
 let part2 m =
-  let ss = starts m 'a' in
+  let ss = starts 'a' m in
   let goal = goal m in
   List.fold_left
     (fun acc start ->
