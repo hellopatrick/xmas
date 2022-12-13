1dcc90277635285e5d28948f5804b00b63a8385e
Patrick Rogers <hellopatrick@users.noreply.github.com>
2022-12-12 23:14:25 -0800

    module

--- day13/main.ml
+++ day13/main.ml
@@ -1,57 +1,52 @@
 open Containers
 
+module Packet = struct
+  type t = Num of int | Lst of t list
+
+  let rec compare t s =
+    match (t, s) with
+    | Num a, Num b ->
+        Int.compare a b
+    | Lst a, Lst b ->
+        compare_lst a b
+    | Num _, Lst _ ->
+        compare (Lst [t]) s
+    | Lst _, Num _ ->
+        compare t (Lst [s])
+
+  and compare_lst a b =
+    match (a, b) with
+    | [], [] ->
+        0
+    | [], _ ->
+        -1
+    | _, [] ->
+        1
+    | hda :: tla, hdb :: tlb ->
+        let res = compare hda hdb in
+        if 0 = res then compare_lst tla tlb else res
+
+  let rec pp t =
+    match t with
+    | Num i ->
+        Printf.sprintf "%d" i
+    | Lst t ->
+        Printf.sprintf "[%s]" (List.map pp t |> String.concat ",")
+end
+
 let input = IO.read_lines_l stdin
 
-type t = Num of int | Lst of t list
-
-let rec comp t s =
-  match (t, s) with
-  | Num a, Num b ->
-      Int.compare a b
-  | Lst a, Lst b ->
-      comp_lst a b
-  | Num _, Lst _ ->
-      comp (Lst [t]) s
-  | Lst _, Num _ ->
-      comp t (Lst [s])
-
-and comp_lst a b =
-  match (a, b) with
-  | [], [] ->
-      0
-  | [], _ ->
-      -1
-  | _, [] ->
-      1
-  | hda :: tla, hdb :: tlb ->
-      let res = comp hda hdb in
-      if 0 = res then comp_lst tla tlb else res
-
-let rec pp t =
-  match t with
-  | Num i ->
-      Printf.sprintf "%d" i
-  | Lst t ->
-      Printf.sprintf "[%s]" (List.map pp t |> String.concat ",")
-
-module P = struct
+module PP = struct
   open Angstrom
   open Xmas.Parsing
+  open Packet
 
-  let o = string "["
-
-  let comma = string ","
-
-  let c = string "]"
-
-  let list = o *> sep_by comma number <* c
-
-  let wrapped p = string "[" *> p <* string "]"
+  let comma = char ','
 
   let f =
     fix (fun packet ->
         let pkt =
-          char '[' *> sep_by comma packet <* char ']' >>| fun l -> Lst l
+          advance 1 *> sep_by comma packet <* char ']' >>| fun l -> Lst l
         in
         let empty = string "[]" >>| fun _ -> Lst [] in
         let num = number >>| fun i -> Num i in
@@ -73,7 +68,7 @@ let parse ls =
     | [] ->
         List.rev out
     | a :: b :: tl ->
-        aux tl ((P.parse a, P.parse b) :: out)
+        aux tl ((PP.parse a, PP.parse b) :: out)
     | hd :: _ ->
         failwith hd
   in
@@ -81,22 +76,23 @@ let parse ls =
 
 let part1 input =
   let pairs = parse input in
-  List.mapi (fun i (a, b) -> if -1 = comp a b then i + 1 else 0) pairs
-  |> Xmas.Enum.sum
+  List.foldi
+    (fun acc i (a, b) -> if Packet.compare a b < 0 then acc + i + 1 else acc)
+    0 pairs
 
 let part2 input =
-  let d2 = P.parse "[[2]]" in
-  let d6 = P.parse "[[6]]" in
+  let d2 = PP.parse "[[2]]" in
+  let d6 = PP.parse "[[6]]" in
   let packets = parse input |> List.flat_map (fun (a, b) -> [a; b]) in
   let packets = d2 :: d6 :: packets in
-  let packets = List.sort comp packets in
+  let packets = List.sort Packet.compare packets in
   let i2 =
-    List.find_idx (fun a -> 0 = comp a d2) packets
+    List.find_idx (fun a -> 0 = Packet.compare a d2) packets
     |> Option.get_exn_or "impossible"
     |> fst
   in
   let i6 =
-    List.find_idx (fun a -> 0 = comp a d6) packets
+    List.find_idx (fun a -> 0 = Packet.compare a d6) packets
     |> Option.get_exn_or "impossible"
     |> fst
   in
