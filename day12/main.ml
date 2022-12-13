7575f18f617c8f52ed89032937273b038be93d82
Patrick Rogers <hellopatrick@users.noreply.github.com>
2022-12-12 17:35:04 -0800

    shave 200ms

--- day12/main.ml
+++ day12/main.ml
@@ -6,15 +6,10 @@ module C = Xmas.Coordinate
 module M = Map.Make (C)
 module S = Set.Make (C)
 
-let parse i =
-  List.foldi
-    (fun acc y line -> String.foldi (fun acc x c -> M.add (x, y) c acc) acc line)
-    M.empty i
-
 let starts c m =
-  m |> M.filter (fun _ h -> Char.equal h c) |> M.keys |> List.of_iter
+  m |> M.filter (fun _ (h, _) -> Char.equal h c) |> M.keys |> List.of_iter
 
-let goal m = m |> M.filter (fun _ h -> Char.equal h 'E') |> M.choose |> fst
+let goal m = m |> M.filter (fun _ (h, _) -> Char.equal h 'E') |> M.choose |> fst
 
 let neighbors (x, y) = [(x - 1, y); (x + 1, y); (x, y - 1); (x, y + 1)]
 
@@ -26,36 +21,52 @@ let height = function
   | c ->
       Char.code c
 
-let get a = Option.get_exn_or "not found" a
+let parse i =
+  let me =
+    List.foldi
+      (fun acc y line ->
+        String.foldi (fun acc x c -> M.add (x, y) c acc) acc line )
+      M.empty i
+  in
+  M.mapi
+    (fun c v ->
+      let h = height v in
+      ( v
+      , List.filter
+          (fun c' ->
+            match M.get c' me with
+            | None ->
+                false
+            | Some h' ->
+                let dh = height h' - h in
+                dh <= 1 )
+          (neighbors c) ) )
+    me
+
+let get ?(def = "not-found") a = Option.get_exn_or def a
 
 let solve m s e =
   let q = Queue.create () in
   let rec aux ds =
-    if Queue.is_empty q then Int.max_int
+    if Queue.is_empty q then None
     else
       let loc = Queue.pop q in
-      let elev = M.get loc m |> get in
-      let dist = M.get loc ds |> get in
+      let dist = M.get loc ds in
       if C.equal loc e then dist
       else
-        let n =
-          List.filter
-            (fun l ->
-              match (M.get l m, M.get l ds) with
-              | None, _ | _, Some _ ->
-                  false
-              | Some elev', _ ->
-                  let dh = height elev' - height elev in
-                  dh <= 1 )
-            (neighbors loc)
-        in
-        Queue.add_seq q (List.to_seq n) ;
-        let ds' =
-          List.fold_left
-            (fun acc c -> M.update c (Fun.const (Some (dist + 1))) acc)
-            ds n
-        in
-        aux ds'
+        match dist with
+        | Some d ->
+            let adj =
+              M.get loc m |> get ~def:"not-found-adj" |> snd
+              |> List.filter (fun c -> M.find_opt c ds |> Option.is_none)
+            in
+            let ds' =
+              List.fold_left (fun acc c -> M.add c (d + 1) acc) ds adj
+            in
+            let _ = Queue.add_seq q (List.to_seq adj) in
+            aux ds'
+        | None ->
+            failwith "impossible"
   in
   let _ = Queue.add s q in
   aux (M.of_list [(s, 0)])
@@ -63,7 +74,7 @@ let solve m s e =
 let part1 m =
   let start = starts 'S' m |> List.hd in
   let goal = goal m in
-  solve m start goal
+  solve m start goal |> get ~def:"part-1"
 
 let part2 m =
   let ss = starts 'a' m in
@@ -71,7 +82,7 @@ let part2 m =
   List.fold_left
     (fun acc start ->
       let steps = solve m start goal in
-      Int.min acc steps )
+      match steps with None -> acc | Some s -> Int.min acc s )
     Int.max_int ss
 
 let _ =
