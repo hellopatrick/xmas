480073ad2016e0589b90901abd630134b5d19783
hellopatrick <sendpatrickmessages@gmail.com>
2023-12-01 00:23:33 -0800

    cleanup

--- 2023/day1/main.ml
+++ 2023/day1/main.ml
@@ -1,20 +1,21 @@
 open Containers
 
 let input = IO.read_lines_l stdin
-let is_numeric c = match c with '0' .. '9' -> true | _ -> false
 
 let outside s =
   let len = String.length s in
-  let first = String.get s 0 in
-  let last = String.get s (len - 1) in
-  Format.sprintf "%c%c" first last
+  if len = 0 then ""
+  else
+    let first = String.get s 0 in
+    let last = String.get s (len - 1) in
+    Format.sprintf "%c%c" first last
 
-let part1 input =
-  List.map (String.filter is_numeric) input
-  |> List.filter Xmas.Str.is_not_empty
-  |> List.map outside
-  |> List.filter_map Int.of_string
-  |> List.fold_left ( + ) 0
+let calibration_value s =
+  s
+  |> String.filter Xmas.Parsing.is_digit
+  |> outside |> Int.of_string |> Option.get_or ~default:0
+
+let part1 input = List.map calibration_value input |> Xmas.Enum.sum
 
 (* well, since words can overlap, let's just sneak the digit into the word at the end,
    since fortunately, no digit spelled out overlaps more than this...*)
@@ -31,11 +32,12 @@ let digits =
     ("nine", "nin9e");
   ]
 
-let replace s =
+let replace_written_digits s =
   List.fold_left (fun s (sub, by) -> String.replace ~sub ~by s) s digits
 
 let part2 input =
-  let clean = List.map replace input in
-  part1 clean
+  input
+  |> List.map replace_written_digits
+  |> List.map calibration_value |> Xmas.Enum.sum
 
 let _ = Printf.printf "part1=%d;part2=%d" (part1 input) (part2 input)
