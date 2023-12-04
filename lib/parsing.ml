0b0117d318602fb32cbdfb6f13a46e3f45c496e7
hellopatrick <sendpatrickmessages@gmail.com>
2023-12-03 22:33:34 -0800

    simplify

--- lib/parsing.ml
+++ lib/parsing.ml
@@ -1,9 +1,8 @@
 open Angstrom
-open Containers
 
 let is_eol = function '\n' | '\r' -> true | _ -> false
 let is_whitespace = function ' ' | '\t' -> true | _ -> false
 let is_digit = function '0' .. '9' -> true | _ -> false
-let number = take_while1 is_digit >>| Int.of_string_exn
+let number = take_while1 is_digit >>| int_of_string
 let whitespace = take_while is_whitespace
 let chomp_eol = take_till is_eol <* end_of_line
