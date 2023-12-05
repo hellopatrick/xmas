b1d28cba09ae91c8af0c43e0a981359c1ca3f4df
hellopatrick <sendpatrickmessages@gmail.com>
2023-12-05 06:02:42 -0800

    dont brute force

--- 2023/day5/main.ml
+++ 2023/day5/main.ml
@@ -2,8 +2,16 @@ open Containers
 
 let input = IO.read_all stdin
 
+module Interval = struct
+  (* Interval.t = [a, b) *)
+  type t = int * int
+
+  let is_empty (a, b) = a >= b
+  let intersects (a, b) (c, d) = if a < c then b > c else a < d
+end
+
 module Input = struct
-  type t = int
+  type t = int * int
 
   open Angstrom
   open Xmas.Parsing
@@ -17,10 +25,10 @@ module Input = struct
     let nums =
       sep_by1 end_of_line
         (list_of_nums >>| function
-         | [ a; b; c ] -> (a, b, c)
+         | [ a; b; c ] -> ((b, b + c), (a, a + c))
          | _ -> failwith "impossible")
     in
-    let block = lift2 (fun s nums -> (s, nums)) header nums in
+    let block = lift2 (fun _ nums -> nums) header nums in
     let* blocks = sep_by1 (many1 end_of_line) block in
     return (prefix, blocks)
 end
@@ -31,26 +39,54 @@ let seeds, maps =
 
 let walk maps seed =
   List.fold_left
-    (fun i (_, map) ->
+    (fun i map ->
       List.fold_while
-        (fun i (dst, src, range) ->
-          if i >= src && i < src + range then
-            let dt = i - src in
-            (dst + dt, `Stop)
-          else (i, `Continue))
+        (fun i ((src, src'), (dst, _)) ->
+          if i >= src && i < src' then (dst + i - src, `Stop) else (i, `Continue))
         i map)
     seed maps
 
 let part1 = List.map (walk maps) seeds |> List.sort Int.compare |> List.hd
 
-let part2 =
-  let chunks = List.chunks 2 seeds in
-  let ranges =
-    List.map
-      (function [ a; b ] -> Seq.range a (a + b) | _ -> failwith "impossible")
-      chunks
+let map_interval maps i =
+  let maps = List.sort (fun ((a, _), _) ((b, _), _) -> Int.compare a b) maps in
+  let rec aux (a, b) acc =
+    if Interval.is_empty (a, b) then acc
+    else
+      match
+        List.find_opt
+          (fun ((a', b'), _) -> Interval.intersects (a, b) (a', b'))
+          maps
+      with
+      | None -> (a, b) :: acc
+      | Some ((ss, se), (ds, de)) ->
+          if a < ss then aux (ss, b) ((a, ss) :: acc)
+          else if b > se then
+            let ds' = a - ss + ds in
+            aux (se, b) ((ds', de) :: acc)
+          else
+            let ds' = a - ss + ds in
+            let de' = ds' + (b - a) in
+            (ds', de') :: acc
   in
-  let seeds = Seq.concat (Seq.of_list ranges) in
-  Seq.map (walk maps) seeds |> Seq.fold min Int.max_int
+  aux i []
+
+let seed_ranges =
+  let rec aux acc = function
+    | [] -> acc
+    | a :: b :: rest -> aux ((a, a + b) :: acc) rest
+    | _ -> failwith "impossible"
+  in
+  aux [] seeds
+
+let part2 =
+  List.map
+    (fun r ->
+      List.fold_left
+        (fun rs maps -> List.flat_map (map_interval maps) rs)
+        [ r ] maps)
+    seed_ranges
+  |> List.flatten
+  |> List.fold_left (fun acc (a, _) -> min acc a) Int.max_int
 
 let _ = Printf.printf "part1=%d;part2=%d" part1 part2
--- 2023/day5/test.t/run.t
+++ 2023/day5/test.t/run.t
@@ -4,4 +4,4 @@ https://adventofcode.com/2023/day/5
   part1=35;part2=46
 
   $ day5.2023 < input
-  part1=551761867;part2=
+  part1=551761867;part2=57451709
