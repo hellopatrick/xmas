65b3776de8ece82415daeef96049695969168f7a
hellopatrick <sendpatrickmessages@gmail.com>
2023-12-08 21:44:53 -0800

    day9

--- 2023/day09/main.ml
+++ 2023/day09/main.ml
@@ -1,5 +1,47 @@
 open Containers
 
-let input = IO.read_lines_l stdin
+module Input = struct
+  let parse line = String.split_on_char ' ' line |> List.map int_of_string
+end
 
-let _ = Printf.printf "part1=;part2="
+let input = IO.read_lines_l stdin
+let series = List.map Input.parse input
+
+let running_diff series =
+  let rec aux acc s =
+    match s with
+    | a :: b :: tl -> aux ((b - a) :: acc) (b :: tl)
+    | _ -> List.rev acc
+  in
+  aux [] series
+
+let derivatives series =
+  let rec aux acc =
+    match acc with
+    | last :: _ ->
+        if List.for_all (Int.equal 0) last then acc
+        else
+          let diffs = running_diff last in
+          aux (diffs :: acc)
+    | [] -> failwith "impossible"
+  in
+  aux [ series ]
+
+let predict series =
+  let ds = derivatives series in
+  let ds = List.map List.rev ds in
+  List.fold_left
+    (fun acc d ->
+      match d with last :: _ -> last + acc | _ -> failwith "impossible")
+    0 ds
+
+let backtrack series =
+  let ds = derivatives series in
+  List.fold_left
+    (fun acc d ->
+      match d with last :: _ -> last - acc | _ -> failwith "impossible")
+    0 ds
+
+let part1 = List.map predict series |> List.fold_left ( + ) 0
+let part2 = List.map backtrack series |> List.fold_left ( + ) 0
+let _ = Printf.printf "part1=%d;part2=%d" part1 part2
--- 2023/day09/test.t/run.t
+++ 2023/day09/test.t/run.t
@@ -1,7 +1,7 @@
 https://adventofcode.com/2023/day/09
 
   $ day09.2023 < test
-  part1=;part2=
+  part1=114;part2=2
 
   $ day09.2023 < input
-  part1=;part2=
+  part1=2098530125;part2=
